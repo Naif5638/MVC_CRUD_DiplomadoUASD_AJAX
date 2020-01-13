d75e83cfe10a3c633bb92017ef713a512bed4b28
Jonathan Lorenzo Contrera <jonathanlorenzoc_rt@outlook.es>
2020-01-13 16:51:19 -0400

    Actualizacion

---
+++ MVC_CRUD_DiplomadoUASD_AJAX/Models/EmpleadoDB.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MVC_CRUD_DiplomadoUASD_AJAX.Models
+{
+    public class EmpleadoDB
+    {
+        string cs = ConfigurationManager.ConnectionStrings["ConnectionStrDB"].ConnectionString;
+
+        public List<Empleado> ListAll()
+        {
+            List<Empleado> lst = new List<Empleado>();
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                SqlCommand command = new SqlCommand("SelectEmpleado", con);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    lst.Add(new Empleado
+                    {
+                        EmpleadoID = Convert.ToInt32(reader["EmpleadoID"]),
+                        Nombres = reader["Nombres"].ToString(),
+                        Apellidos = reader["Apellidos"].ToString(),
+                        Edad = Convert.ToInt32(reader["Edad"]),
+                        Estado_Civil = reader["Estado_Civil"].ToString(),
+                        Pais = reader["Pais"].ToString(),
+                    });
+                }
+                return lst;
+            }
+        }
+
+        public int Add(Empleado empleado)
+        {
+            int i;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                SqlCommand command = new SqlCommand("InsertUpdateEmpleado", con);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@Id", empleado.EmpleadoID);
+                command.Parameters.AddWithValue("@Nombres", empleado.Nombres);
+                command.Parameters.AddWithValue("@Apellidos", empleado.Apellidos);
+                command.Parameters.AddWithValue("@Edad", empleado.Edad);
+                command.Parameters.AddWithValue("@Estado_Civil", empleado.Estado_Civil);
+                command.Parameters.AddWithValue("@Pais", empleado.Pais);
+                command.Parameters.AddWithValue("@Action", "Insert");
+
+                i = command.ExecuteNonQuery();
+            }
+            return i;
+        }
+
+        //Metodo para 
+        public int Update(Empleado empleado)
+        {
+            int i;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                SqlCommand command = new SqlCommand("InsertUpdateEmpleado", con);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@Id", empleado.EmpleadoID);
+                command.Parameters.AddWithValue("@Nombres", empleado.Nombres);
+                command.Parameters.AddWithValue("@Apellidos", empleado.Apellidos);
+                command.Parameters.AddWithValue("@Edad", empleado.Edad);
+                command.Parameters.AddWithValue("@Estado_Civil", empleado.Estado_Civil);
+                command.Parameters.AddWithValue("@Pais", empleado.Pais);
+                command.Parameters.AddWithValue("@Action", "Update");
+
+                i = command.ExecuteNonQuery();
+            }
+            return i;
+        }
+
+        public int Delete(int id)
+        {
+            int i;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                SqlCommand command = new SqlCommand("DeleteEmpleado", con);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@Id", id);
+                i = command.ExecuteNonQuery();
+            }
+            return i;
+        }
+    }
+}
---
+++ MVC_CRUD_DiplomadoUASD_AJAX/Scripts/employee.js
@@ -0,0 +1,198 @@
+﻿$(document).ready(function () {
+    loadData();
+    console.log("Hola")
+});
+
+function loadData() {
+    $.ajax({
+        url: '/Home/List',
+        type: 'GET',
+        contentType: "application/json; charset-utf-8",
+        dataType: "json",
+        sucesss: function (result) {
+            var html = '';
+            console.log("HOLA");
+            $.each(result, function (key, item) {
+                html += '<tr>';
+                html += '<td>' + item.EmpleadoId + '</dt>';
+                html += '<td>' + item.Nombres + '</dt>';
+                html += '<td>' + item.Apellidos + '</dt>';
+                html += '<td>' + item.Edad + '</dt>';
+                html += '<td>' + item.Estado_Civil + '</dt>';
+                html += '<td>' + item.Pais + '</dt>';
+                html += '<td><a href="#" onClick="return getbyId(' + item.EmpleadoId + ')">Edit</a> | <a href="#" onClick="Delete(' + item.EmpleadoId + ')">Delete</a></dt>';
+                html += '</tr>';
+            });
+            $('.tbody').html(html);
+        },
+        error: function (errormessEdad) {
+            alert(errormessEdad.responseText);
+        }
+    });
+}
+
+function Add() {
+    var res = validate();
+    if (res == false) {
+        return false;
+    }
+    var empleadoObject = {
+        EmpleadoID: $('#EmpleadoID').val(),
+        Nombres: $('#Nombres').val(),
+        Apellidos: $('#Apellidos').val(),
+        Edad: $('#Edad').val(),
+        Estado_Civil: $('#Estado_Civil').val(),
+        Pais: $('#Pais').val()
+    };
+    $.ajax({
+        url: '/Home/Add',
+        data: JSON.stringify(empleadoObject),
+        type: 'POST',
+        contentType: "application/json;charset=utf-8",
+        datatype: "json",
+        success: function (result) {
+            loadData();
+            $('#myModal').modal('hide');
+        },
+        error: function (errormessEdad) {
+            alert(errormessEdad.responseText);
+        }
+    });
+}
+
+function getbyId(id) {
+    $('#Nombres').css('border-color', 'lightgrey');
+    $('#Edad').css('border-color', 'lightgrey');
+    $('#Estado_Civil').css('border-color', 'lightgrey');
+    $('#Pais').css('border-color', 'lightgrey');
+    $.ajax({
+        url: "/Home/getbyId/" + id,
+        type: "GET",
+        contentType: "application/json;charset=utf-8",
+        dataType: 'json',
+        success: function (result) {
+            $('#EmpleadoId').val(result.EmpleadoId);
+            $('#Nombres').val(result.Nombres);
+            $('#Apellidos').val(result.Apellidos);
+            $('#Edad').val(result.Edad);
+            $('#Estado_Civil').val(result.Estado_Civil);
+            $('#Pais').val(result.Pais);
+            $('#myModal').modal('show');
+            $('#btnUpdate').show();
+            $('#btnAdd').hide();
+        },
+        error: function (errormessEdad) {
+            alert(errormessEdad.responseText);
+        }
+    });
+    return false;
+}
+
+
+function Update() {
+    var res = validate();
+    if (res == false) {
+        return false;
+    }
+    var empleadoObject = {
+        EmpleadoID: $('#EmpleadoID').val(),
+        Nombres: $('#Nombres').val(),
+        Apellidos: $('#Apellidos').val(),
+        Edad: $('#Edad').val(),
+        Estado_Civil: $('#Estado_Civil').val(),
+        Pais: $('#Pais').val()
+    };
+    $.ajax({
+        url: '/Home/Update',
+        data: JSON.stringify(empleadoObject),
+        type: 'POST',
+        contentType: "application/json;charset=utf-8",
+        datatype: "json",
+        success: function (result) {
+            loadData();
+            $('#myModal').modal('hide');
+            $('#EmpleadoId').val("");
+            $('#Nombres').val("");
+            $('#Apellidos').val("");
+            $('#Edad').val("");
+            $('#Estado_Civil').val("");
+            $('#Pais').val("");
+        },
+        error: function (errormessEdad) {
+            alert(errormessEdad.responseText);
+        }
+    });
+}
+
+function Delete(id) {
+    var ans = confirm("Are you sure want to delete this record?");
+    if (ans) {
+        $.ajax({
+            url: "/Home/Delete/" + id,
+            type: "POST",
+            contentType: "application/json; charset = utf-8",
+            dataType: "json",
+            success: function (result) {
+                loadData();
+            },
+            error: function (errormessEdad) {
+                alert(errormessEdad.responseText);
+            }
+        });
+    }
+}
+
+function clearTextBox() {
+    $('#EmpleadoId').val("");
+    $('#Nombres').val("");
+    $('#Apellidos').val("");
+    $('#Edad').val("");
+    $('#Estado_Civil').val("");
+    $('#Pais').val("");
+    $('#btnUpdate').hide();
+    $('#btnAdd').show();
+    $('#Nombres').css('border-color', 'lightgrey');
+    $('#Edad').css('border-color', 'lightgrey');
+    $('#Estado_Civil').css('border-color', 'lightgrey');
+    $('#Pais').css('border-color', 'lightgrey');
+}
+
+function validate() {
+    var isValid = true;
+    if ($('#Nombres').val().trim() == "") {
+        $('#Nombres').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#Nombres').css('border-color', 'lightgrey');
+    }
+    if ($('#Apellidos').val().trim() == "") {
+        $('#Apellidos').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#Apellidos').css('border-color', 'lightgrey');
+    }
+    if ($('#Edad').val().trim() == "") {
+        $('#Edad').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#Edad').css('border-color', 'lightgrey');
+    }
+    if ($('#Estado_Civil').val().trim() == "") {
+        $('#Estado_Civil').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#Estado_Civil').css('border-color', 'lightgrey');
+    }
+    if ($('#Pais').val().trim() == "") {
+        $('#Pais').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#Pais').css('border-color', 'lightgrey');
+    }
+    return isValid;
+}
